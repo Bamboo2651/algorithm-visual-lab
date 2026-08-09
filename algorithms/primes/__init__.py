a379fd39a55f7a943213110e7317dc0e2782df06
Bamboo2651 <tah331925@gmail.com>
2026-08-09 20:25:49 +0900

    refactor: 素数探索一覧をレジストリへ集約

--- algorithms/primes/__init__.py
+++ algorithms/primes/__init__.py
@@ -1,2 +1,13 @@
 """素数探索アルゴリズム。"""
 
+from .sieve import ALGORITHM as SIEVE
+from .trial_division import ALGORITHM as TRIAL_DIVISION
+
+
+ALL_ALGORITHMS = [
+    TRIAL_DIVISION,
+    SIEVE,
+]
+
+
+__all__ = ["ALL_ALGORITHMS"]
