563289b4710da19f1e2e9fc2fb9d772a044cad1c
Bamboo2651 <tah331925@gmail.com>
2026-08-09 20:26:41 +0900

    refactor: 数列一覧をレジストリへ集約

--- algorithms/sequences/__init__.py
+++ algorithms/sequences/__init__.py
@@ -1,2 +1,13 @@
 """数列を生成するアルゴリズム。"""
 
+from .collatz import ALGORITHM as COLLATZ
+from .recaman import ALGORITHM as RECAMAN
+
+
+ALL_ALGORITHMS = [
+    RECAMAN,
+    COLLATZ,
+]
+
+
+__all__ = ["ALL_ALGORITHMS"]
